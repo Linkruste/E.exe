913dbf2491f3773a6690a91a0dcf1591c6eaf2a0
Linkruste <74532664+Linkruste@users.noreply.github.com>
2022-07-02 16:55:19 +0200

    E 0.5
    E plays well but in an external music file (that can be found in the "bin/Release/" folder, the zip archive is ready-to-use).
    Oh and Windows Defender and Malwarebytes consider this as a virus that connects you to a botnet but don't worry the app is for offline use.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace E
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string localPath = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location),"e.wav");
+        //private string localPath = $"../../e.wav";
+        public MainWindow()
+        {
+            this.ShowInTaskbar = true;
+            InitializeComponent();
+            //MessageBox.Show(localPath);
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(localPath);
+            player.Play();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left) this.DragMove();
+            MainWindow win = new MainWindow();
+            win.Show();
+        }
+    }
+}
